ea828e58c8f9642cc0d2d59c9ad4c84a64261f78
Nick Hird <nickdh94@gmail.com>
2018-02-11 19:31:35 +0000

    Started experiments in regards to procedual generations
    project for this branch is runninng on unity 2017.3.1

---
+++ Assets/Scripts/Models/Block.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Block : MonoBehaviour {
+	public bool Breakable = false;
+	public int X { private set; get; }
+	public int Y { private set; get; }
+
+	public void SetPosition(int x, int y) {
+		X = x;
+		Y = y;
+	}
+	
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
