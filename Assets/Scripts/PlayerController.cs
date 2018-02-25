f3fef05117833ea1083374da3934c164a84d020f
Unknown <anusmita.ray@gmail.com>
2018-02-25 22:41:18 +0000

    Created basic navigation for player
    Player moves around obstacles

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(PlayerMoving))]
+public class PlayerController : MonoBehaviour {
+
+    PlayerMoving moving;
+    Camera cam;
+	// Use this for initialization
+	void Start () {
+        cam = Camera.main;
+        moving = GetComponent<PlayerMoving>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+
+            if(Physics.Raycast(ray,out hit))
+            {
+                //check if player detected another player
+                //interact accordingly
+
+                moving.MoveTo(hit.point);
+            }
+        }
+		
+	}
+}
