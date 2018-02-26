7a21f21383aa793c3e49f6cecbd4f7e9f5a0a60c
Unknown <anusmita.ray@gmail.com>
2018-02-26 12:45:26 +0000

    Creepy Enemy Follows Player Around
    As long as player is in creepy guy's radar, he gets followed around. When player's not in the range anymore, enemy stops.

--- Assets/Scripts/PlayerMoving.cs
+++ Assets/Scripts/PlayerMoving.cs
@@ -7,6 +7,7 @@ using UnityEngine.AI;
 public class PlayerMoving : MonoBehaviour {
 
     NavMeshAgent agent;
+	GameObject player;
 
 	// Use this for initialization
 	void Start () {
@@ -16,10 +17,31 @@ public class PlayerMoving : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		
+		if (player != null) {
+			MoveTo (player.transform.position);
+		}
 	}
 
     public void MoveTo(Vector3 point)
     {
         agent.SetDestination(point);
     }
+
+	public void OnTriggerEnter(Collider col)
+	{
+		if (col.transform.CompareTag ("Player"))
+		{
+			player = col.gameObject;
+		}
+	}
+
+	public void OnTriggerExit(Collider col)
+	{
+		Debug.Log ("Not Being Triggered By ... " + col.gameObject.name);
+		if (col.transform.CompareTag ("Player"))
+		{
+			agent.SetDestination (transform.position);
+			player = null;
+		}
+	}
 }
