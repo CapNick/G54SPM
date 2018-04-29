f45abbdbbabff7da53bca6a5993f64eb3b49f982
Nick Hird <nickdh94@gmail.com>
2018-04-29 17:26:06 +0100

    Auto stash before merge of "Experimental" and "origin/Experimental"

--- Assets/Scripts/World/Features/Tree.cs
+++ Assets/Scripts/World/Features/Tree.cs
@@ -1,5 +1,13 @@
-﻿namespace World.Features {
+﻿using UnityEngine;
+
+namespace World.Features {
     public class Tree {
-        
+        public void CreateTreeType1(Map map, Vector3 position, int leafType, int woodType) {
+            map.AddBlock(position, woodType);
+            map.AddBlock(new Vector3(position.x, position.y+1, position.z), woodType);
+            map.AddBlock(new Vector3(position.x, position.y+2, position.z), woodType);
+            map.AddBlock(new Vector3(position.x, position.y+3, position.z), woodType);
+            
+        }
     }
 }
