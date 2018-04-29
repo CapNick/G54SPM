2cd134838f3b292218ac4987f4f15e49afae53ec
Joshua Charles <joshcharles96@gmail.com>
2018-04-29 22:32:26 +0100

    Trying to differentiate sounds of surfaces for footsteps

--- Assets/Footstep.cs
+++ Assets/Footstep.cs
@@ -20,6 +20,8 @@ public class Footstep : MonoBehaviour {
 		if (cc.isGrounded == true && cc.velocity.magnitude > 2f && GetComponent<AudioSource>().isPlaying == false) 
 		{
 			GetComponent<AudioSource>().Play();
+			//FindObjectOfType<SFXAudioManager>().play("Grass_step");
+
 		}
 	}
 }
