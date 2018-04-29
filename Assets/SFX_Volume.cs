17346e8fab5be39879afad7ad7681c93ce280c38
Joshua Charles <joshcharles96@gmail.com>
2018-04-29 22:36:51 +0100

    Creating AudioManager script

---
+++ Assets/SFX_Volume.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SFX_Volume : MonoBehaviour {
+
+	public Slider volume;
+	public AudioSource Mymusic;
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+}
